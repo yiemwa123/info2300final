88c1c98bec383f39b920034d0f7db5863018ea24
Emma Wang <yw345@cornell.edu>
2020-04-20 01:25:22 -0400

    created print block

--- includes/navbar.php
+++ includes/navbar.php
@@ -1,7 +1,6 @@
 <div class="heading">
   <!-- Source: https://filmschoolrejects.com/the-movies-of-studio-ghibli-ranked-from-worst-to-best-b480bffd7fb7/-->
   <div class="logo">
-    <img src="images/menu.png" alt="Menu" />
     <a href="index.php">
       <img src="images/ghiblilogo.jpg" alt="Studio Ghibli Logo">
     </a>
@@ -11,7 +10,12 @@
     <div class="group">
       <form id="ghiblisearch" action="index.php" method="get" novalidate>
         <div class="search">
-          <input class="bar" name='moviesearch' type="text" placeholder="Search a Studio Ghibli Movie" />
+          <select name="field">
+            <option value="title">Title</option>
+            <option value="tags">Keywords</option>
+          </select>
+          <label for="bar"></label>
+          <input id="bar" class="bar" name='moviesearch' type="text" placeholder="Search a Studio Ghibli Movie" />
           <!-- Source: https://thenounproject.com/search/?q=search%20icon&i=2443377-->
           <button class="button" type="submit"><img src="images/searchicon.png" alt="Search Icon"></button>
         </div>
@@ -20,3 +24,15 @@
     </div>
   </div>
 </div>
+
+<?php
+
+// This function prints out a block with a photo and movie name formatted properly
+function printblock($img, $moviename){?>
+
+<div class="block">
+  <img src=<?php echo($img);?> alt="Theatrical poster for <?php echo ($moviename)?>"/>
+  <p> <?php echo ($moviename)?> </p>
+</div>
+
+<?php }?>
--- index.php
+++ index.php
@@ -10,17 +10,11 @@ if (isset($field)) {
 };
 $valid_field = inlist($field, listofoptions);
 
+?>
 
-// This function creates the dropdown list for when the user wants to search through different fields
-function createoptions($listofoptions)
-{ ?>
-  <select name="field">
-    <?php foreach ($listofoptions as $option => $value) { ?>
-      <option value="<?php echo $value; ?>"><?php echo $option; ?></option>
-    <?php } ?>
-  </select>
-<?php }
 
+
+<?php
 // This function returns true if the field is a valid field, else otherwise
 function inlist($field, $listoffields)
 {
@@ -32,44 +26,32 @@ function inlist($field, $listoffields)
 } ?>
 
 
-<?php
 
-// This function prints all the records
-
-
-// This function checks to see if the inputs for the insert form are valid
-function inputvalid($movie_name, $year, $rating, $award, $synopsis)
-{
-  if (!empty($year) && strlen($award) > 0) {
-    settype($year, "integer");
-    settype($award, "integer");
-    if (!empty($movie_name) && $year < 2019 && $year > 1980 && !empty($rating) && !empty($synopsis) && $award == 0 || $award == 1) {
-      return TRUE;
-    } else {
-      return FALSE;
-    }
-  } else {
-    return FALSE;
-  }
-}
-
-?>
 <!DOCTYPE html>
 <html lang="en">
 
 <head>
-    <meta charset="UTF-8">
-    <title>Studio Ghibli</title>
-    <link rel="stylesheet" type="text/css" href="styles/site.css" media="all" />
+  <meta charset="UTF-8">
+  <title>Studio Ghibli</title>
+  <link rel="stylesheet" type="text/css" href="styles/site.css" media="all" />
 </head>
 
 <body>
-<?php include("includes/navbar.php") ?>
+  <?php include("includes/navbar.php") ?>
 
-<h2> Top 3 Box Office Hits </h2>
-<h2> Newest Releases </h2>
+  <div class="content">
 
-<?php include("includes/footer.php") ?>
+    <h2> Top 3 Box Office Hits </h2>
+
+    <?php printblock("images/studiologo.jpg", "Test") ?>
+
+    <h2> Newest Releases </h2>
+
+  </div>
+
+
+
+  <?php include("includes/footer.php") ?>
 </body>
 
 </html>
